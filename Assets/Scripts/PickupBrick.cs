4a34f44d7eb43f2449da8e531c826478ac765da4
FinnT <FinnT@LAPPI-XMG-WIN>
2025-09-09 19:36:07 +0200

    Finn: Asset Nailgun, pickup Brick script

---
+++ Assets/Scripts/PickupBrick.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+
+public class PickupBrick : MonoBehaviour
+{
+    private bool canPickUp;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        canPickUp = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (canPickUp && Input.GetKey(KeyCode.E))
+        {
+            // Destroy the brick
+            Destroy(gameObject);
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        // Check if the colliding object has the "Player" tag
+        if (other.CompareTag("Player"))
+        {
+            canPickUp = true;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        canPickUp = false;
+    }
+}
