7ee2d6fa7e7cefe15368738d594bcb11097356ca
FinnT <FinnT@LAPPI-XMG-WIN>
2025-09-10 16:24:05 +0200

    Finn: Kamerasteuerung

--- Assets/Scripts/PickupBrick.cs
+++ Assets/Scripts/PickupBrick.cs
@@ -13,7 +13,7 @@ public class PickupBrick : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (canPickUp && Input.GetKey(KeyCode.E))
+        if (canPickUp && Input.GetKeyDown(KeyCode.E))
         {
             // Destroy the brick
             Destroy(gameObject);
